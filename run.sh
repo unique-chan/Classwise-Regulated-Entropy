3bbf54bd9d06eb3b255c08c8c699e435d036fdfb
unique-chan <yechankim@gm.gist.ac.kr>
2020-12-18 16:00:23 +0900

    Remove a dummy dataset folder

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 train.py --network_name='resnet18' --dataset_dir='./chan' --height='899' --width='1920'
+python3 train.py --network_name='resnet18' --dataset_dir='../chan' --height='899' --width='1920'
