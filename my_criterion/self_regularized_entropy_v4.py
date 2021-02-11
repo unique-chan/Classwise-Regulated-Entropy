eb0d11ca9a1dff8c7e4f3982f4c10742aebc3a42
unique-chan <yechankim@gm.gist.ac.kr>
2021-02-11 16:59:23 +0900

    self-regularized-entropy-v4

--- my_criterion/self_regularized_entropy_v4.py
+++ my_criterion/self_regularized_entropy_v4.py
@@ -15,21 +15,20 @@ class SelfRegularizedEntropy(nn.Module):
         batch_size = len(y)                                          # N
         yHat = F.softmax(yHat, dim=1)
 
-        yHat_child = torch.ones_like(yHat) * self.psi
+        psi_distribution = torch.ones_like(yHat) * self.psi
         yHat_zerohot = torch.ones(batch_size, self.C).scatter_(1, y.view(batch_size, 1).data.cpu(), 0)
-        norm = yHat + yHat_child * self.K + 1e-10
+        norm = yHat + psi_distribution * self.K + 1e-10
         # e = - (yHat / norm) log (yHat / norm)
         classwise_entropy = (yHat / norm) * torch.log((yHat / norm) + 1e-10)
         # e += - K * ( (ψ / norm) log (ψ / norm) )
-        classwise_entropy += ((yHat_child / norm) * torch.log((yHat_child / norm) + 1e-10)) * self.K
-        # e = e ⊙ yHat (⊙: Hadamard Product)
-        classwise_entropy *= yHat
+        classwise_entropy += ((psi_distribution / norm) * torch.log((psi_distribution / norm) + 1e-10)) * self.K
+        # e = e ⊙ (yHat + γ) (⊙: Hadamard Product)
+        gamma = 0.5
+        classwise_entropy *= (yHat + gamma)
         # e = e ⊙ yHat_zerohot (To ignore all ground truth classes)
         classwise_entropy *= yHat_zerohot.cuda()
         # e = scalar_sum(e)
         entropy = float(torch.sum(classwise_entropy))
         # e = e / N
         entropy /= batch_size
-        # e = e / K
-        entropy /= self.K
         return entropy
