ff7ce546756a6b5421f51c1a96f4a68add6d3a05
unique-chan <yechankim@gm.gist.ac.kr>
2021-02-24 15:58:39 +0900

    Modify (2) CE (Complement Entropy) for multi-gpu

--- my_criterion/CE.py
+++ my_criterion/CE.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 
 
 class ComplementEntropy(nn.Module):
-    def __init__(self, num_classes, device):
-        self.classes = num_classes                      # C (number of classes)
+    def __init__(self, C, device):
+        self.C = C                                      # C (number of classes)
         self.device = device                            # {'cpu', 'cuda:0', 'cuda:1', ...}
         super(ComplementEntropy, self).__init__()
 
@@ -16,10 +16,10 @@ class ComplementEntropy(nn.Module):
         Yg_ = (1 - Yg) + 1e-7                           # numerical trick
         Px = yHat / Yg_.view(len(yHat), 1)
         Px_log = torch.log(Px + 1e-10)
-        y_zerohot = torch.ones(batch_size, self.classes).scatter_(
+        y_zerohot = torch.ones(batch_size, self.C).scatter_(
             1, y.view(batch_size, 1).data.cpu(), 0)
         output = Px * Px_log * y_zerohot.to(device=self.device)
         loss = torch.sum(output)
         loss /= float(batch_size)
-        loss /= float(self.classes)
+        loss /= float(self.C)
         return loss
