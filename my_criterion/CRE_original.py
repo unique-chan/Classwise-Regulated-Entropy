4b980f2357471e73f76afa3a8282bb60c8917b6a
unique-chan <yechankim@gm.gist.ac.kr>
2021-03-11 00:18:48 +0900

    Add CNN Models - densenet, efficientnet, squeezenet

--- my_criterion/CRE_original.py
+++ my_criterion/CRE_original.py
@@ -23,20 +23,14 @@ class ClasswiseRegulatedEntropy(nn.Module):
 
         batch_size = len(y)                                          # N
         yHat = F.softmax(yHat, dim=1)
-        # print('예찬 yHat', yHat.shape)
         psi_distribution = torch.ones_like(yHat) * self.psi
-        # print('예찬 psi_distribution', psi_distribution.shape)
         yHat_zerohot = torch.ones(batch_size, self.C).scatter_(1, y.view(batch_size, 1).data.cpu(), 0)
-        # print('예찬 yHat_zerohot', yHat_zerohot.shape)
         norm = yHat + psi_distribution * self.K + 1e-10
-        # print('예찬 norm', norm.shape)
         classwise_entropy = (yHat / norm) * torch.log((yHat / norm) + 1e-10)
-        # print('예찬 classwise_entropy', classwise_entropy.shape)
         classwise_entropy += ((psi_distribution / norm) * torch.log((psi_distribution / norm) + 1e-10)) * self.K
-        # gamma = 0.3
-        # classwise_entropy *= (yHat + gamma)
+        gamma = 0.3
+        classwise_entropy *= (yHat + gamma)
         classwise_entropy *= yHat_zerohot.to(device=self.device)
         entropy = float(torch.sum(classwise_entropy))
-        # print('예찬 entropy', entropy.shape)
         entropy /= batch_size
         return entropy
--- my_utils/model.py
+++ my_utils/model.py
@@ -1,7 +1,7 @@
 import torchvision.models as models
 import torch.nn as nn
 from my_utils.my_models import resnet_for_tiny as rst
-# from my_utils.my_models import resnext_for_tiny as rsxt
+from my_utils.my_models.efficientnet import EfficientNet
 
 
 def model(network_name, num_classes, pretrained=False):
@@ -17,22 +17,7 @@ def model(network_name, num_classes, pretrained=False):
 # Ref.1: https://pytorch.org/docs/stable/torchvision/models.html
 # Ref.2: https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
 # Ref.3: https://stackoverflow.com/questions/63015883/pytorch-based-resnet18-achieves-low-accuracy-on-cifar100
-
-
-# def ResNeXt29_2x64d(num_classes, pretrained):
-#     return rsxt.ResNeXt29_2x64d(num_classes=num_classes)
-#
-#
-# def ResNeXt29_4x64d(num_classes, pretrained):
-#     return rsxt.ResNeXt29_4x64d(num_classes=num_classes)
-#
-#
-# def ResNeXt29_8x64d(num_classes, pretrained):
-#     return rsxt.ResNeXt29_8x64d(num_classes=num_classes)
-#
-#
-# def ResNeXt29_32x4d(num_classes, pretrained):
-#     return rsxt.ResNeXt29_32x4d(num_classes=num_classes)
+# Ref.4: https://nannow.github.io/pytorch/2019/01/08/Pytorch-5.html
 
 
 def resnet18_for_tiny(num_classes, pretrained):
@@ -82,6 +67,93 @@ def resnet101(num_classes, pretrained):
 def resnet152(num_classes, pretrained):
     net = models.resnet152(pretrained)
     net.fc = nn.Linear(net.fc.in_features, num_classes)
+    return net
+
+
+def squeezenet(num_classes, pretrained):
+    net = models.squeezenet1_0(pretrained)
+    net.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+    net.num_classes = num_classes
+    return net
+
+
+def efficientnet_b0(num_classes, pretrained):
+    net_name = 'efficientnet-b0'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b1(num_classes, pretrained):
+    net_name = 'efficientnet-b1'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b2(num_classes, pretrained):
+    net_name = 'efficientnet-b2'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b3(num_classes, pretrained):
+    net_name = 'efficientnet-b3'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b4(num_classes, pretrained):
+    net_name = 'efficientnet-b4'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b5(num_classes, pretrained):
+    net_name = 'efficientnet-b5'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b6(num_classes, pretrained):
+    net_name = 'efficientnet-b6'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def efficientnet_b7(num_classes, pretrained):
+    net_name = 'efficientnet-b7'
+    net = EfficientNet.from_pretrained(net_name) if pretrained \
+        else EfficientNet.from_name(net_name)
+    return net
+
+
+def densenet121(num_classes, pretrained):
+    net = models.densenet121(pretrained)
+    net.fc = nn.Linear(net.classifier.in_features, num_classes)
+    return net
+
+
+def densenet161(num_classes, pretrained):
+    net = models.densenet161(pretrained)
+    net.fc = nn.Linear(net.classifier.in_features, num_classes)
+    return net
+
+
+def densenet169(num_classes, pretrained):
+    net = models.densenet169(pretrained)
+    net.fc = nn.Linear(net.classifier.in_features, num_classes)
+    return net
+
+
+def densenet201(num_classes, pretrained):
+    net = models.densenet201(pretrained)
+    net.fc = nn.Linear(net.classifier.in_features, num_classes)
     return net
 
 
